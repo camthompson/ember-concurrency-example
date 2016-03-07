d2913e2e9adf9540e60cb492e71be7f6a6a8f1ee
Cam Thompson <cam@camthompson.com>
2016-03-07 11:25:56 -0500

    Add bad counter component

---
+++ app/components/x-counter-bad.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+const {
+  Component,
+  run
+} = Ember;
+
+export default Component.extend({
+  count: 0,
+
+  init() {
+    this._super(...arguments);
+    this.incrementCount();
+  },
+
+  incrementCount() {
+    this.incrementProperty('count');
+    run.later(_ => this.incrementCount(), 1000);
+  }
+});
